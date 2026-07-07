66aab751de72eb39a4fdf20e97d9e66576dc6e7a
Hynjun_Oh <dhguswns75@gmail.com>
2026-07-07 11:59:28 +0900

    Pin deployed API origin to HTTPS gateway

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,4 @@
-const DEPLOYED_API_ORIGIN = "https://wwfgppukzf.execute-api.ap-northeast-2.amazonaws.com";
-const envApiOrigin = import.meta.env.VITE_API_BASE_URL;
-
-export const API_ORIGIN =
-    envApiOrigin?.startsWith("http://growlab-backend.") ? DEPLOYED_API_ORIGIN : envApiOrigin || DEPLOYED_API_ORIGIN;
+export const API_ORIGIN = "https://wwfgppukzf.execute-api.ap-northeast-2.amazonaws.com";
 export const API_BASE = `${API_ORIGIN}/api`;
 
 export const serverUrl = (path = "") => `${API_ORIGIN}${path}`;
