dadd3d3afea21a923eaea014dfdd89330b9c58d3
Hynjun_Oh <dhguswns75@gmail.com>
2026-07-07 12:23:08 +0900

    Fix API Gateway URL

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const API_ORIGIN = "https://wwfgppukzf.execute-api.ap-northeast-2.amazonaws.com";
+export const API_ORIGIN = "https://wwfggpukzf.execute-api.ap-northeast-2.amazonaws.com";
 export const API_BASE = `${API_ORIGIN}/api`;
 
 export const serverUrl = (path = "") => `${API_ORIGIN}${path}`;
